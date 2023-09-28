166f74528a122142185b88c87c3119013ed609c5
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-09-28 13:16:58 -0400

    Modified to better match BitstreamEvolution config

--- config.py
+++ config.py
@@ -32,9 +32,11 @@ dpg.create_context()
 # toggle state of the sender changes. Usually used for checkboxes.
 def cb_handle_enable_state_on_toggle(_, enabled, item):
   if enabled:
-    dpg.enable_item(item)
+    #dpg.enable_item(item)
+    dpg.show_item(item)
   else:
-    dpg.disable_item(item)
+    #dpg.disable_item(item)
+    dpg.hide_item(item)
 
 with dpg.item_handler_registry(tag="show_item_on_click") as handler:
   def cb_show_item_on_click(_, click_event_data):
@@ -60,11 +62,11 @@ def cb_handle_select_one_file(_, file_selection_data, write_loc):
 
 ### Start creating GUI ###
 
-dpg.create_viewport(title="Config Editor", width=700, height=700,
-                    max_width=700, min_width=700, max_height=700,
-                    min_height=700)
+dpg.create_viewport(title="Config Editor", width=1000, height=700)#,
+                    #max_width=700, min_width=700, max_height=700,
+                    #min_height=700)
 
-with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
+with dpg.window(label="", width=1000, height=700, no_resize=True, no_move=True,
                 no_collapse=True, no_title_bar=True):
   ### Section for configuring operation mode ###
   with dpg.collapsing_header(label="Operation Mode"):
@@ -86,32 +88,25 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
     def cb_update_fit_fn(sender, fitness_function):
       dpg.delete_item("fit_fn_param")
       
-      if fitness_function == "Variance":
-        with dpg.group(horizontal=True, tag="fit_fn_param",
+      if fitness_function == "Combined":
+        with dpg.group(tag="fit_fn_param",
                         before="fit_fn_param_anchor"):
-          dpg.add_text(fit_justified_strs["Variance Weight: "])
-          dpg.add_slider_float(min_value=0, max_value=1.0, clamped=True)
-      elif fitness_function == "Pulse":
-        with dpg.group(horizontal=True, tag="fit_fn_param",
-                        before="fit_fn_param_anchor"):
-          dpg.add_text(fit_justified_strs["Pulse Weight: "])
-          dpg.add_slider_float(min_value=0, max_value=1.0, clamped=True)
-      elif fitness_function == "Combined":
-        with dpg.group(horizontal=True, tag="fit_fn_param",
-                        before="fit_fn_param_anchor"):
-          dpg.add_text(fit_justified_strs["Combination Type: "])
-          dpg.add_radio_button(["Add", "Mult"], horizontal=True)
-      else:
-        raise Exception("You broke my app :(")
+          with (dpg.group(horizontal=True)):
+            dpg.add_text(fit_justified_strs["Combination Type: "])
+            dpg.add_radio_button(["Add", "Mult"], horizontal=True)
+          
+          with (dpg.group(horizontal=True)):
+            dpg.add_text(fit_justified_strs["Variance Weight: "])
+            dpg.add_slider_float(min_value=0, max_value=5.0, clamped=True)
+
+          with (dpg.group(horizontal=True)):
+            dpg.add_text(fit_justified_strs["Pulse Weight: "])
+            dpg.add_slider_float(min_value=0, max_value=5.0, clamped=True)
 
     dpg.add_text("Fitness Function:")
     dpg.add_radio_button(["Variance", "Pulse", "Combined"],
                           default_value="Variance", callback=cb_update_fit_fn,
                           horizontal=True)
-
-    with dpg.group(horizontal=True, tag="fit_fn_param"):
-      dpg.add_text(fit_justified_strs["Variance Weight: "])
-      dpg.add_slider_float(min_value=0, max_value=1.0, clamped=True)
 
     dpg.add_separator(tag="fit_fn_param_anchor")
 
@@ -157,12 +152,12 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
           dpg.add_text("Selection Type:")
           dpg.add_radio_button(["Single Elite", "Fractional Elite",
                                 "Classic Tournament",
-                                "Fitness Proportional", "Rank Proportional"],
+                                "Fitness Proportional", "Rank Proportional", "MAP Elites"],
                                 default_value="Fitness Proportional")
 
         with dpg.table_cell():
           dpg.add_text("Diversity Measure:")
-          dpg.add_radio_button(["None", "Unique", "Hamming Distance"],
+          dpg.add_radio_button(["None", "Unique Individuals", "Hamming Distance"],
                                 default_value="Hamming Distance")
 
     dpg.add_separator()
@@ -183,6 +178,7 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
                         user_data="fitness_target")
       dpg.add_input_int(min_value=2, min_clamped=True, default_value=1000,
                         enabled=False, tag="fitness_target")
+      dpg.hide_item("fitness_target")
 
   ### Section for configuring initialization mode ###
   with dpg.collapsing_header(label="Initialization"):
@@ -190,14 +186,27 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
     # responsible for showing/hiding the randomization parameters everytime the
     # "Randomize" option is selected/deselected.
     def cb_handle_init_mode_change(sender, init_mode):
+      dpg.delete_item("randomize_params")
+
       if init_mode == "Randomize":
         with dpg.group(horizontal=True, tag="randomize_params",
                         before="randomize_params_anchor"):
           dpg.add_text("Randomize: ")
           dpg.add_combo(["until pulse threshold", "until variance threshold",
                            "once"], default_value="once")
-      else:
-        dpg.delete_item("randomize_params")
+      elif init_mode == "Use existing population":
+        with dpg.group(horizontal=True, tag="existing_pop_params",
+                        before="randomize_params_anchor"):
+          item = "Populations Source: "
+          dpg.add_text(item)
+          dpg.add_input_text(readonly=True, tag=f"{item} display",
+                              user_data=f"{item} selector")
+          dpg.bind_item_handler_registry(f"{item} display", "show_item_on_click")
+          with dpg.file_dialog(show=False, directory_selector=True, modal=True,
+                                width=500, height=300, tag=f"{item} selector",
+                                user_data=f"{item} display",
+                                callback=cb_handle_select_one_file):
+            dpg.add_file_extension(".*")
 
     with dpg.group():
       dpg.add_text("Initialization Mode:")
@@ -220,8 +229,7 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
                                           "Save .asc files to: ",
                                           "Save .bin files to: ",
                                           "Save MCU data to: ",
-                                          "Analysis directory: ",
-                                          "Populations source: ")
+                                          "Analysis directory: ")
 
     with dpg.group(horizontal=True):
       dpg.add_text(outp_justified_strs["Log level: "])
@@ -266,7 +274,7 @@ with dpg.window(label="", width=700, height=700, no_resize=True, no_move=True,
     # Non-optional output directories
     items = ["Save Output to: ", "Save .asc files to: ",
              "Save .bin files to: ", "Save MCU data to: ",
-             "Analysis directory: ", "Populations source: "]
+             "Analysis directory: "]
     for item in items:
       with dpg.group(horizontal=True):
         dpg.add_text(outp_justified_strs[item])
